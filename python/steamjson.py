df2a81a7a07948322028b3b11fb275bd7f3961d7
azukizuke <azukizuke@gmail.com>
2020-04-13 16:24:59 +0900

    a

--- python/steamjson.py
+++ python/steamjson.py
@@ -1,4 +1,21 @@
 import sys
 
+
+class SteamJson:
+
+    def __init__(self, leagueid, apikey):
+        self.root = {}
+        self._leagueid = leagueid
+        self._apikey = apikey
+        ####
+        self.make_matchid_json()
+
+    def make_matchid_json(self):
+        has_next = False
+        while(not has_next):
+            print("test")
+            has_next = True
+
+
 if __name__ == "__main__":
     pass
