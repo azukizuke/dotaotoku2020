8da7a86616fea59b42174bee1ab1ca453311cd20
azukizuke <azukizuke@gmail.com>
2020-04-13 16:24:51 +0900

    a

--- python/make_league_stat.py
+++ python/make_league_stat.py
@@ -1,5 +1,10 @@
 import sys
+import steamjson
 
 if __name__ == "__main__":
+    leagueid = sys.argv[1]
+    apikey = sys.argv[2]
+
     print("--start--")
+    steamjson = steamjson.SteamJson(leagueid, apikey)
     print("--end--")
