a9c12f3583cf92fbe583dfa6609ec0b591de87b7
azukizuke <azukizuke@gmail.com>
2020-04-13 17:36:34 +0900

    2020-04-13-1736 otsukare

--- python/steamjson.py
+++ python/steamjson.py
@@ -1,7 +1,15 @@
 import sys
-
+import url
+import json
 
 class SteamJson:
+    _STEAMAPI_BASE = ("http://api.steampowered.com/"
+                      "/IDOTA2Match_570"
+                      "/GetMatchHistory/v1")
+    _STEAMAPI_LEAGUE = "?league_id="
+    _STEAMAPI_KEY = "&key="
+    _STEAMAPI_REQUEST = "&matches_requested="
+    _STEAMAPI_MAXREQUEST = 300
 
     def __init__(self, leagueid, apikey):
         self.root = {}
@@ -11,10 +19,19 @@ class SteamJson:
         self.make_matchid_json()
 
     def make_matchid_json(self):
-        has_next = False
-        while(not has_next):
-            print("test")
-            has_next = True
+        has_next = True
+        while(has_next):
+            has_next = False
+            result={}
+            _url = (self._STEAMAPI_BASE
+                    + self._STEAMAPI_LEAGUE
+                    + str(self._leagueid)
+                    + self._STEAMAPI_KEY
+                    + str(self._apikey)
+                    + self._STEAMAPI_REQUEST
+                    + str(self._STEAMAPI_MAXREQUEST))
+            result = url.get_url(_url)
+            print(json.dumps(result, indent = 4))
 
 
 if __name__ == "__main__":
