d9896e0be4570736a79af6f1f9c254c662346d18
azukizuke <azukizuke@gmail.com>
2020-04-15 16:12:25 +0900

    save

--- python/hero.py
+++ python/hero.py
@@ -3,3 +3,9 @@ class Hero:
     def __init__(self, heroid, name):
         self.heroid = heroid
         self.name = name
+        self.pickbans = {]
+        self._init_pickbans()
+
+    def _init_pickbans(self)
+        pass
+        
